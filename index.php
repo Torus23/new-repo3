4757a8272ee90b5f1e825f82b0ac131455d92b3a
Logan Bull <107020194+Torus23@users.noreply.github.com>
2023-10-29 00:36:49 +1300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     $db_name = "shopping";
     $conn = mysqli_init();
     mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306,MYSQLI_CLIENT_SSL));
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306,MYSQLI_CLIENT_SSL);
     if (mysqli_connect_errno($conn)) {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
